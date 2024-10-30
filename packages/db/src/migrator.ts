39742e50a2b47ba48b7a1ac4428be85a468eae27
mackie <macklinu@users.noreply.github.com>
2024-10-30 13:46:52 -0400

    Refactor migrator sql statements

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -1,6 +1,68 @@
 import { Sql } from 'postgres'
 import { createPostgres } from './create-postgres.js'
 
+const createTeamsTable = (sql: Sql) => sql`
+  CREATE TABLE IF NOT EXISTS teams (
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    team_id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    city TEXT NOT NULL,
+    abbreviation TEXT NOT NULL
+  )
+`
+
+const createGamesTable = (sql: Sql) => sql`
+  CREATE TABLE IF NOT EXISTS games (
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    game_id TEXT PRIMARY KEY,
+    game_status TEXT NOT NULL,
+    home_team_id INTEGER NOT NULL REFERENCES teams (team_id),
+    visitor_team_id INTEGER NOT NULL REFERENCES teams (team_id)
+  )
+`
+
+const createTeamGameMetadataTable = (sql: Sql) => sql`
+  CREATE TABLE IF NOT EXISTS team_game_metadata (
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    game_id TEXT NOT NULL REFERENCES games (game_id),
+    team_id INTEGER NOT NULL REFERENCES teams (team_id),
+    record TEXT NOT NULL DEFAULT '0-0',
+    score INTEGER NOT NULL DEFAULT 0
+  )
+`
+
+const addTeamGameMetadataCompositeKey = (sql: Sql) => [
+  sql`
+    ALTER TABLE IF EXISTS team_game_metadata
+    DROP CONSTRAINT IF EXISTS team_game_metadata_pkey
+  `,
+  sql`
+    ALTER TABLE IF EXISTS team_game_metadata
+    ADD CONSTRAINT team_game_metadata_pkey PRIMARY KEY (game_id, team_id)
+  `,
+]
+
+const addTeamGameMetadataIndices = (sql: Sql) => [
+  sql`
+    CREATE INDEX IF NOT EXISTS team_game_metadata_game_id_index ON team_game_metadata (game_id)
+  `,
+  sql`
+    CREATE INDEX IF NOT EXISTS team_game_metadata_team_id_index ON team_game_metadata (team_id)
+  `,
+]
+
+const addGamesIndices = (sql: Sql) => [
+  sql`
+    CREATE INDEX IF NOT EXISTS games_home_team_id_index ON games (home_team_id)
+  `,
+  sql`
+    CREATE INDEX IF NOT EXISTS games_visitor_team_id_index ON games (visitor_team_id)
+  `,
+]
+
 export class Migrator {
   private sql: Sql
 
@@ -10,56 +72,12 @@ export class Migrator {
 
   async applyMigrations() {
     await this.sql.begin((sql) => [
-      sql`
-        CREATE TABLE IF NOT EXISTS teams (
-          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          team_id INTEGER NOT NULL PRIMARY KEY,
-          name TEXT NOT NULL,
-          city TEXT NOT NULL,
-          abbreviation TEXT NOT NULL
-        )
-      `,
-      sql`
-        CREATE TABLE IF NOT EXISTS games (
-          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          game_id TEXT PRIMARY KEY,
-          game_status TEXT NOT NULL,
-          home_team_id INTEGER NOT NULL REFERENCES teams (team_id),
-          visitor_team_id INTEGER NOT NULL REFERENCES teams (team_id)
-        )
-      `,
-      sql`
-        CREATE TABLE IF NOT EXISTS team_game_metadata (
-          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          game_id TEXT NOT NULL REFERENCES games (game_id),
-          team_id INTEGER NOT NULL REFERENCES teams (team_id),
-          record TEXT NOT NULL DEFAULT '0-0',
-          score INTEGER NOT NULL DEFAULT 0
-        )
-      `,
-      sql`
-        ALTER TABLE IF EXISTS team_game_metadata
-        DROP CONSTRAINT IF EXISTS team_game_metadata_pkey
-      `,
-      sql`
-        ALTER TABLE IF EXISTS team_game_metadata
-        ADD CONSTRAINT team_game_metadata_pkey PRIMARY KEY (game_id, team_id)
-      `,
-      sql`
-        CREATE INDEX IF NOT EXISTS team_game_metadata_game_id_index ON team_game_metadata (game_id)
-      `,
-      sql`
-        CREATE INDEX IF NOT EXISTS team_game_metadata_team_id_index ON team_game_metadata (team_id)
-      `,
-      sql`
-        CREATE INDEX IF NOT EXISTS games_home_team_id_index ON games (home_team_id)
-      `,
-      sql`
-        CREATE INDEX IF NOT EXISTS games_visitor_team_id_index ON games (visitor_team_id)
-      `,
+      createTeamsTable(sql),
+      createGamesTable(sql),
+      createTeamGameMetadataTable(sql),
+      ...addTeamGameMetadataCompositeKey(sql),
+      ...addTeamGameMetadataIndices(sql),
+      ...addGamesIndices(sql),
     ])
   }
 }
